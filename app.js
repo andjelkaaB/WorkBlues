d8f4a69e51c071b2a18bd6b504eaa4b0d661add6
AndjelkaaB <andjelkaabulatovic@gmail.com>
2023-03-13 00:31:18 +0100

    nmg

--- app.js
+++ app.js
@@ -1,127 +1,260 @@
-class Employee {
-    constructor(fullName, email, phoneNumber, dateOfBirth, monthlySalary) {
-        this.fullName = fullName;
-        this.email = email;
-        this.phoneNumber = phoneNumber;
-        this.dateOfBirth = dateOfBirth;
-        this.monthlySalary = monthlySalary
-    }
-}
-
-const allEmployees = []
-const form = document.querySelector("#employee-form")
-let employeeContainer = document.querySelector('.employee-container')
-let employeeProfile
-let empoloyee1 = new Employee("Andjelka Bulatovic", "andjelkaa@gmail.com", "+3815646546","28.1.1999", "1000")
-allEmployees.push(empoloyee1)
-
-
-const employeeList = JSON.parse(localStorage.getItem("employees"))
-
-
-function addEmployee(fullName, email, phoneNumber, dateOfBirth, monthlySalary) {
-    const newEmployee = new Employee(fullName, email, phoneNumber, dateOfBirth, monthlySalary)
-
-    allEmployees.push(newEmployee)
-
-    // employeeList = JSON.parse(localStorage.getItem("employees"))
-}
-
-form.addEventListener('submit', function(event) {
-    event.preventDefault()
-
-    let fullName = document.querySelector('#name').value
-    let email = document.querySelector('#email').value
-    let phoneNumber = document.querySelector('#phone-number').value
-    let dateOfBirth = document.querySelector('#birth-date').value
-    let monthlySalary = document.querySelector('#salary').value
-
-    addEmployee(fullName, email, phoneNumber, dateOfBirth, monthlySalary)
-
-    document.querySelector('#name').value = ''
-    document.querySelector('#email').value = ''
-    document.querySelector('#phone-number').value = ''
-    document.querySelector('#birth-date').value = ''
-    document.querySelector('#salary').value = ''
-
-
-    const allEmployeesJSON = localStorage.setItem('employees', (JSON.stringify(allEmployees)))
-    
-
-    displayEmployees()
-    }
-)
-
-function displayEmployees() {
-
-    employeeList.forEach((employee, index) => {
-        employeeProfile = document.createElement('div')
-        employeeProfile.classList.add('employee')
-
-        employeeProfile.innerHTML = `
-        <div>
-            <span class='label'>Full name:</span> ${employee.fullName}
-        </div>
-        <div>
-            <span class='label'>E-mail:</span> ${employee.email}
-        </div>
-        <div>
-            <span class='label'>Phone number:</span> ${employee.phoneNumber}
-        </div>
-        <div>
-            <span class='label'>Date of birth:</span> ${employee.dateOfBirth}
-        </div>
-        <div>
-            <span class='label'>Monthly salary:</span> ${employee.monthlySalary}$
-        </div>
-        `
-
-        employeeContainer.appendChild(employeeProfile)
-
-        addDeleteButton(employeeProfile, index)
-    })
-
-}
-displayEmployees()
-
-function addDeleteButton(employeeProfile, index){
-    const deleteButton = document.createElement('button')
-    deleteButton.classList.add('delete-employee-button')
-    deleteButton.textContent = 'Delete'
-    employeeProfile.appendChild(deleteButton)
-
-    deleteButton.addEventListener("click", () => {
-        employeeList.splice(index, 1)
-
-        localStorage.setItem('employees', JSON.stringify(employeeList))
-
-        employeeProfile.remove()
-    })
-}
-
 const openSidebarButton = document.querySelector('.openbtn')
 const sidebar = document.querySelector('.sidebar')
 
 openSidebarButton.addEventListener('click', () => {
     sidebar.classList.toggle('show')
-    console.log(openSidebarButton)
 })
 
 function closeNav() {
     document.querySelector(".sidebar").classList.remove('show')
 }
 
-function openForm() {
-    document.querySelector("#overlay").style.display = "block"
-    document.querySelector("#overlay").style.height = "100%"
+
+let employeeList = []
+let employeeTableContent
+
+
+function newEmployee() {
+    let name = document.getElementById('name').value
+    let id = document.getElementById('id').value
+    let email = document.getElementById('email').value
+    let phone = document.getElementById('phone').value
+    let birthday = document.getElementById('birthday').value
+    let salary = document.getElementById('salary').value
+
+    if (localStorage.getItem("employeeList") == null) {
+        employeeList = []
+    } else {
+        employeeList = JSON.parse(localStorage.getItem("employeeList"))
+    }
+
+    employeeList.unshift({
+        name: name,
+        id: id,
+        email: email,
+        phone: phone,
+        birthday: birthday,
+        salary: salary
+    })
+    localStorage.setItem("employeeList", (JSON.stringify(employeeList)))
+
+    showEmployeeData()
+
+    document.getElementById('name').value = ''
+    document.getElementById('id').value = ''
+    document.getElementById('email').value = ''
+    document.getElementById('phone').value = ''
+    document.getElementById('birthday').value = ''
+    document.getElementById('salary').value = ''
 }
 
-function closeForm() {
-    document.querySelector("#overlay").style.height = "0%";
+
+
+function showEmployeeData() {
+    if (localStorage.getItem('employeeList') == null) {
+        employeeList = []
+    } else {
+        employeeList = JSON.parse(localStorage.getItem('employeeList'))
+    }
+
+    employeeTableContent = ''
+
+    employeeList.forEach((employeeList, index) => {
+        employeeTableContent += `<tr>
+            <td>${employeeList.name}</td>
+            <td>${employeeList.id}</td>
+            <td>${employeeList.email}</td>
+            <td>${employeeList.phone}</td>
+            <td>${employeeList.birthday}</td>
+            <td>${employeeList.salary}</td>
+            <td>
+            <button onclick="deleteEmployee(${index})" class="delete-btn">Delete</button>
+            <button onclick="editEmployee(${index})" class="edit-btn">Edit</button>
+            </td>
+        </tr>`
+
+    })
+
+    document.querySelector("#employee-table-body").innerHTML = employeeTableContent
+
 }
 
-// create a form to edit current employee data
+function deleteEmployee(index) {
+    employeeList = JSON.parse(localStorage.getItem('employeeList'))
+    employeeList.splice(index, 1)
+    localStorage.setItem('employeeList', JSON.stringify(employeeList))
+    showEmployeeData()
+}
 
-//proceed to tasks
 
-//maybe integrate a seacrh task function to see whos working on it as addition
+function editEmployee(index) {
+    document.getElementById('submit-button').style.display = "none"
+    document.getElementById('update-btn').style.display = "block"
+
+    employeeList = JSON.parse(localStorage.getItem('employeeList'))
+    let employee = employeeList[index]
+
+    document.getElementById('name').value = employee.name
+    document.getElementById('id').value = employee.id
+    document.getElementById('id').readOnly = true
+    document.getElementById('email').value = employee.email
+    document.getElementById('phone').value = employee.phone
+    document.getElementById('birthday').value = employee.birthday
+    document.getElementById('salary').value = employee.salary
+
+    document.getElementById('update-btn').onclick = function (){
+        employeeList[index].name = document.getElementById('name').value
+        employeeList[index].id = document.getElementById('id').value
+        employeeList[index].email = document.getElementById('email').value
+        employeeList[index].phone = document.getElementById('phone').value
+        employeeList[index].birthday = document.getElementById('birthday').value
+        employeeList[index].salary = document.getElementById('salary').value
+
+        localStorage.setItem('employeeList', JSON.stringify(employeeList))
+        document.getElementById('id').readOnly = false
+        showEmployeeData()
+
+        document.getElementById('name').value = ''
+        document.getElementById('id').value = ''
+        document.getElementById('email').value = ''
+        document.getElementById('phone').value = ''
+        document.getElementById('birthday').value = ''
+        document.getElementById('salary').value = ''
+    }
+    document.getElementById('submit-button').style.display = "block"
+    document.getElementById('update-btn').style.display = "none"
+}
+
+let taskList = []
+let task 
+let tasksTableContent
+
+
+function newTask() {
+    let title = document.getElementById('title').value
+    let description = document.getElementById('description').value
+    let asignee = document.getElementById('asignee-name').value
+    let dueDate = document.getElementById('due-date').value
+    let status = document.getElementById('status').value = "in progress"
+
+    if (localStorage.getItem('taskList') == null){
+        taskList = []
+    } else {
+        taskList = JSON.parse(localStorage.getItem('taskList'))
+    }
+
+    taskList.unshift({
+        title: title,
+        description: description,
+        asignee: asignee,
+        dueDate: dueDate,
+        status: status
+    })
+
+    localStorage.setItem('taskList', JSON.stringify(taskList))
+
+    showTaskList()
+
+    document.getElementById('title').value = ''
+    document.getElementById('description').value = ''
+    document.getElementById('asignee-name').value = ''
+    document.getElementById('due-date').value = ''
+    document.getElementById('status').value = 'In progress'
+}
+
+
+function showTaskList() {
+    if (localStorage.getItem('taskList') == null){
+        taskList = []
+    } else {
+        taskList = JSON.parse(localStorage.getItem('taskList'))
+    }
+
+    tasksTableContent = ''
+
+    taskList.forEach((task, index) => {
+        tasksTableContent += `<tr>
+            <td>${task.title}</td>
+            <td>${task.description}</td>
+            <td>${task.asignee}</td>
+            <td>${task.dueDate}</td>
+            <td>${task.status}</td>
+            <button onclick="deleteTask(${index})" class="delete-btn">Delete</button>
+            <button onclick="editTask(${index})" class="edit-btn">Edit</button>
+            <button onclick="completeTask(${index})" class="status">Done</button>
+
+        </tr>`
+    })
+
+    document.querySelector("#task-table-body").innerHTML = tasksTableContent
+}
+
+function deleteTask(index) {
+    taskList = JSON.parse(localStorage.getItem('taskList'))
+    taskList.splice(index, 1)
+    localStorage.setItem('taskList', JSON.stringify(taskList))
+    showTaskList()
+}
+
+function editTask(index) {
+    document.getElementById('submit-button').style.display = "none"
+    document.getElementById('update-btn').style.display = "block"
+
+    taskList = JSON.parse(localStorage.getItem('taskList'))
+    task = taskList[index]
+
+    document.getElementById('title').value = task.title
+    // document.getElementById('id').value = task.id
+    // document.getElementById('id').readOnly = true
+    document.getElementById('description').value = task.description
+    document.getElementById('asignee-name').value = task.asignee
+    document.getElementById('due-date').value = task.due-date
+    document.getElementById('status').value = task.status
+
+    document.getElementById('update-btn').onclick = function (){
+        taskList[index].title = document.getElementById('title').value
+        // taskList[index].id = document.getElementById('id').value
+        taskList[index].description = document.getElementById('description').value
+        taskList[index].asignee = document.getElementById('asignee-name').value
+        taskList[index].dueDate = document.getElementById('due-date').value
+        taskList[index].status = document.getElementById('status').value
+
+        localStorage.setItem('taskList', JSON.stringify(taskList))
+        showTaskList()
+
+        document.getElementById('title').value = ''
+        // document.getElementById('id').value = ''
+        document.getElementById('description').value = ''
+        document.getElementById('asignee-name').value = ''
+        document.getElementById('due-date').value = ''
+        document.getElementById('status').value = ''
+    }
+    document.getElementById('submit-button').style.display = "block"
+    document.getElementById('update-btn').style.display = "none"
+}
+
+
+function completeTask(index){
+        taskList[index].status = document.getElementById('status').value = 'Completed'
+        localStorage.setItem('taskList', JSON.stringify(taskList))
+        showTaskList()
+        document.getElementById('status').value = ''
+    }
+
+
+if (window.location.pathname === '/employee-profile.html') {
+    window.onload = showEmployeeData()
+    document.getElementById('employee-form').addEventListener('submit', (event) => {
+        event.preventDefault()
+        newEmployee()
+    })
+} else if (window.location.pathname === '/tasks.html') {
+    window.onload = showTaskList()
+    document.getElementById('tasks-form').addEventListener('submit', (event) => {
+        event.preventDefault()
+        newTask()
+    })
+} else {
+
+}
+
